928f4287b0c2e75e9754e3da9786591bc9022c25
Sebastiamg <sebasortiz112004@gmail.com>
2023-03-31 11:52:18 -0500

    letra

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Header from './Components/Header'
 
 const App = () => {
   return (
     <>
       {/* header */}
-      
+      <Header />
       {/* Body */}
 
       {/* Footer */}
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+    <div className='font1'>
+        <h1 className='text-slate-100 bg-purple-800 h-28 font-bold'>Citas Yavirac</h1>    
+    </div>
+)
 }
 
 export default Header
