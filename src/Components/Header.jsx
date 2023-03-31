48992edb216095859ebf465e88b9dfac2b68369d
Sebastiamg <sebasortiz112004@gmail.com>
2023-03-31 12:06:20 -0500

    Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='font1'>
-        <h1 className='text-slate-100 bg-purple-800 h-28 font-bold'>Citas Yavirac</h1>    
+        <h1 className='text-slate-100 bg-purple-800 h-20 font-bold flex justify-center p-5 text-3xl'>Citas Yavirac</h1>    
+        <section className='bg-purple-900 text-slate-100 flex justify-evenly p-2 h-16 shadow-md shadow-gray-900'>
+            <button className='hover:font-bold hover:bg-purple-800 p-1 rounded-lg transition-all'>
+                Inicio <i class="fa-sharp fa-solid fa-caret-down"></i>
+                </button>
+            <button className='hover:font-bold hover:bg-purple-800 p-1 rounded-lg transition-all'>
+                Menú <i class="fa-solid fa-compass"></i>
+                </button>
+            <button className='hover:font-bold hover:bg-purple-800 p-1 rounded-lg transition-all'>
+                Nosotros <i class="fa-solid fa-users"></i>
+                </button>
+        </section>
     </div>
 )
 }
